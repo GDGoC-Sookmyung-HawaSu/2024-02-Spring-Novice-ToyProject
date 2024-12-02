38e84fe59c02d4527c205666a89dbb794fc0abbc
ha9eun <ha9eunsoo@gmail.com>
2024-12-02 20:47:24 +0900

    Feat: 마이페이지 내가 쓴 글 섹션 추가

--- 하은수/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/MyPageController.java
+++ 하은수/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/MyPageController.java
@@ -3,19 +3,27 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoldu.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @RequiredArgsConstructor
 @Controller
 public class MyPageController {
+
+    private final PostsService postsService;
+
     @GetMapping("/mypage")
     public String myPage(Model model, @LoginUser SessionUser user) {
         model.addAttribute("userName", user.getName());
         model.addAttribute("userEmail", user.getEmail());
         model.addAttribute("userPicture", user.getPicture());
+
+        model.addAttribute("posts", postsService.findPostsByUser(user.getEmail()));
         return "my-page";
     }
 
